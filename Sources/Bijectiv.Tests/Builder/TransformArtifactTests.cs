5a06faffd0572c9911094f54082d2e391dc78565
TheMouthOfACow <brian.tyler@gmail.com>
2014-04-03 22:04:30 +0100

    The git add in doesnt add all files by default. grrr.

---
+++ Sources/Bijectiv.Tests/Builder/TransformArtifactTests.cs
@@ -0,0 +1,206 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TransformArtifactTests.cs" company="Bijectiv">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 Brian Tyler
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Defines the TransformArtifactTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable AssignNullToNotNullAttribute
+namespace Bijectiv.Tests.Builder
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+
+    using Bijectiv.Builder;
+    using Bijectiv.Builder.Fakes;
+    using Bijectiv.Tests.Tools;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// This class tests the <see cref="TransformArtifact"/> class.
+    /// </summary>
+    [TestClass]
+    public class TransformArtifactTests
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void CreateInstance_ValidParameters_InstanceCreated()
+        {
+            // Arrange
+
+            // Act
+            new TransformArtifact(typeof(object), typeof(object)).Naught();
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void CreateInstance_ValidParameters_ArtifactIsEmpty()
+        {
+            // Arrange
+
+            // Act
+            var target = new TransformArtifact(typeof(object), typeof(object));
+
+            // Assert
+            Assert.IsFalse(target.Any());
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateInstance_SourceParameterIsNull_Throws()
+        {
+            // Arrange
+
+            // Act
+            new TransformArtifact(null, typeof(object)).Naught();
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateInstance_TargetParameterIsNull_Throws()
+        {
+            // Arrange
+
+            // Act
+            new TransformArtifact(typeof(object), null).Naught();
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void CreateInstance_SourceParameter_IsAssignedToSourceProperty()
+        {
+            // Arrange
+
+            // Act
+            var target = new TransformArtifact(typeof(int), typeof(object));
+
+            // Assert
+            Assert.AreEqual(typeof(int), target.Source);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void CreateInstance_TargetParameter_IsAssignedToTargetProperty()
+        {
+            // Arrange
+
+            // Act
+            var target = new TransformArtifact(typeof(object), typeof(int));
+
+            // Assert
+            Assert.AreEqual(typeof(int), target.Target);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Add_FragmentParameterIsNull_Throws()
+        {
+            // Arrange
+            var target = CreateTarget();
+
+            // Act
+            target.Add(null);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Add_ValidFragment_IsAddedToCollection()
+        {
+            // Arrange
+            var target = CreateTarget();
+            var fragment = new StubTransformFragment(typeof(object), typeof(object));
+
+            // Act
+            target.Add(fragment);
+
+            // Assert
+            Assert.AreEqual(fragment, target.Single());
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_FragmentSourceMismatch_Throws()
+        {
+            // Arrange
+            var target = CreateTarget();
+            var fragment = new StubTransformFragment(typeof(int), typeof(object));
+
+            // Act
+            target.Add(fragment);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_FragmentTargetMismatch_Throws()
+        {
+            // Arrange
+            var target = CreateTarget();
+            var fragment = new StubTransformFragment(typeof(object), typeof(int));
+
+            // Act
+            target.Add(fragment);
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Collection_ViewedAsNonGeneric_IsExposed()
+        {
+            // Arrange
+            var target = CreateTarget();
+            var fragment = new StubTransformFragment(typeof(object), typeof(object));
+
+            // Act
+            target.Add(fragment);
+
+            // Assert
+            // ReSharper disable once RedundantCast
+            Assert.AreEqual(fragment, ((IEnumerable)target).Cast<TransformFragment>().Single());
+        }
+
+        private static TransformArtifact CreateTarget()
+        {
+            return new TransformArtifact(typeof(object), typeof(object));
+        }
+    }
+}
---
+++ Sources/Bijectiv/Builder/TransformArtifact.cs
@@ -0,0 +1,159 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TransformArtifact.cs" company="Bijectiv">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 Brian Tyler
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Defines the TransformArtifact type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Bijectiv.Builder
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// An artifact that completely describes a transform.
+    /// </summary>
+    public class TransformArtifact : IEnumerable<TransformFragment>
+    {
+        /// <summary>
+        /// The fragments that comprise the artifact.
+        /// </summary>
+        private readonly List<TransformFragment> fragments = new List<TransformFragment>();
+
+        /// <summary>
+        /// The source type.
+        /// </summary>
+        private readonly Type source;
+
+        /// <summary>
+        /// The target type.
+        /// </summary>
+        private readonly Type target;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="TransformArtifact"/> class.
+        /// </summary>
+        /// <param name="source">
+        /// The source.
+        /// </param>
+        /// <param name="target">
+        /// The target.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when any parameter is null.
+        /// </exception>
+        public TransformArtifact([NotNull] Type source, [NotNull] Type target)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (target == null)
+            {
+                throw new ArgumentNullException("target");
+            }
+
+            this.source = source;
+            this.target = target;
+        }
+
+        /// <summary>
+        /// Gets the source type.
+        /// </summary>
+        public Type Source
+        {
+            get { return this.source; }
+        }
+
+        /// <summary>
+        /// Gets the target type.
+        /// </summary>
+        public Type Target
+        {
+            get { return this.target; }
+        }
+
+        /// <summary>
+        /// Adds the fragment to the artifact.
+        /// </summary>
+        /// <param name="fragment">
+        /// The fragment.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when any parameter is null.
+        /// </exception>
+        public void Add([NotNull] TransformFragment fragment)
+        {
+            if (fragment == null)
+            {
+                throw new ArgumentNullException("fragment");
+            }
+
+            if (fragment.Source != this.Source)
+            {
+                throw new ArgumentException(string.Format(
+                    "The fragment source '{0}' does not match the artifact source '{1}'", 
+                    fragment.Source,
+                    this.Source));
+            }
+
+            if (fragment.Target != this.Target)
+            {
+                throw new ArgumentException(string.Format(
+                    "The fragment target '{0}' does not match the artifact target '{1}'",
+                    fragment.Target,
+                    this.Target));
+            }
+
+            this.fragments.Add(fragment);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the fragments in the artifact.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the fragments in the collection.
+        /// </returns>
+        public IEnumerator<TransformFragment> GetEnumerator()
+        {
+            return this.fragments.GetEnumerator();
+        }
+        
+        /// <summary>
+        /// Returns an enumerator that iterates through the fragments in the artifact.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the fragments in the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.fragments.GetEnumerator();
+        }
+    }
+}
