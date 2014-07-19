fdaddf4cf1cc7f1ff30fc9a15b8b3a3a4416c13c
TheMouthOfACow <brian.tyler@gmail.com>
2014-07-19 22:07:59 +0100

    Include lost file MemberValueSourceTransformSubtaskTests.

---
+++ Sources/Bijectiv.Tests/KernelFactory/MemberValueSourceTransformSubtaskTests.cs
@@ -0,0 +1,101 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MemberValueSourceTransformSubtaskTests.cs" company="Bijectiv">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 Brian Tyler
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Defines the MemberValueSourceTransformSubtaskTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Bijectiv.Tests.KernelFactory
+{
+    using Bijectiv.Configuration;
+    using Bijectiv.KernelFactory;
+    using Bijectiv.TestUtilities;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// This class tests the <see cref="MemberValueSourceTransformSubtask"/> class.
+    /// </summary>
+    [TestClass]
+    public class MemberValueSourceTransformSubtaskTests
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void CreateInstance_DefaultParameters_InstanceCreated()
+        {
+            // Arrange
+
+            // Act
+            new MemberValueSourceTransformSubtask().Naught();
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ArgumentNullExceptionExpected]
+        public void ProcessShard_ScaffoldParameterIsNull_Throws()
+        {
+            // Arrange
+            var target = new MemberValueSourceTransformSubtask();
+
+            // Act
+            // ReSharper disable once AssignNullToNotNullAttribute
+            target.ProcessShard(null, Stub.Create<MemberFragment>(), Stub.Create<ValueSourceMemberShard>());
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ArgumentNullExceptionExpected]
+        public void ProcessShard_FragmentParameterIsNull_Throws()
+        {
+            // Arrange
+            var target = new MemberValueSourceTransformSubtask();
+
+            // Act
+            // ReSharper disable once AssignNullToNotNullAttribute
+            target.ProcessShard(Stub.Create<InjectionScaffold>(), null, Stub.Create<ValueSourceMemberShard>());
+
+            // Assert
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ArgumentNullExceptionExpected]
+        public void ProcessShard_ShardParameterIsNull_Throws()
+        {
+            // Arrange
+            var target = new MemberValueSourceTransformSubtask();
+
+            // Act
+            // ReSharper disable once AssignNullToNotNullAttribute
+            target.ProcessShard(Stub.Create<InjectionScaffold>(), Stub.Create<MemberFragment>(), null);
+
+            // Assert
+        }
+    }
+}
