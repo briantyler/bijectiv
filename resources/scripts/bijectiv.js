81d44bc7c724d09b72b66bf76d4333eac5ffa94d
TheMouthOfACow <brian.tyler@gmail.com>
2014-09-01 20:00:42 +0100

    Removed old info.

--- resources/scripts/bijectiv.js
+++
@@ -1,9 +0,0 @@
-function bijectiv()
-{
-    // A cross platform hack to get the Linx font size the same as windows
-    if (navigator.platform.indexOf("Linux")!=-1)
-    {
-        $(".highlight").css("font-size", "1.1em");
-    }
-}
-
